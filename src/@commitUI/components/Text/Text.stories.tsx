34fdc85ca94ab30593223ee3a6183b80bda54dd4
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-14 01:29:12 +0800

    Added stories for Text component

--- src/@commitUI/components/Text/Text.stories.tsx
+++ src/@commitUI/components/Text/Text.stories.tsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react";
+import React from "react"
+import { Story, Meta } from "@storybook/react"
 
-import { Text, Props } from ".";
+import { Text, Props } from "."
 
 export default {
-    title: "Components/Text",
-    component: Text,
-} as Meta;
+  title: "Components/Text",
+  component: Text,
+} as Meta
 
-const Template: Story<Props> = (args) => <Text {...args} />;
+const Template: Story<Props> = (args) => <Text {...args} />
 
-export const Heading1 = Template.bind({});
-Heading1.args = {
-    children: "Heading 1",
-};
+export const Base = Template.bind({})
+Base.args = {
+  children: "Base",
+}
+
+export const SemiBold = Template.bind({})
+SemiBold.args = {
+  children: "Semi Bold",
+  semibold: true,
+}
+
+export const Bold = Template.bind({})
+Bold.args = {
+  children: "Bold",
+  bold: true,
+}
+
+export const Ellipsize = Template.bind({})
+Ellipsize.args = {
+  children:
+    "Ellipsize is the three dots at the end. This is a super long text. This is a super long text. This is a super long text. This is a super long text. This is a super long text. This is a super long text. ",
+  ellipsize: true,
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  children: "size=sm. You can try other sizes as well.",
+  size: "sm",
+}
+
+export const Warning = Template.bind({})
+Warning.args = {
+  children: "type=warning. You can try other types(colors) as well.",
+  type: "warning",
+}
