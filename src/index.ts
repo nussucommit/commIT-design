2fa9928cae39368d02a0baf945a129c8dd3fc045
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-07-15 01:17:49 +0800

    Add entry point of package

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { Text } from "./commit-ui/components/Text"
+export { Heading } from "./commit-ui/components/Heading"
+export { Button } from "./commit-ui/components/Button"
+export type { Props as ButtonProps } from "./commit-ui/components/Button"
+export {
+  SidebarContainer,
+  SidebarLink,
+  SidebarMenu,
+  SidebarWrapper,
+} from "./commit-ui/components/Sidebar"
+export { Nav, NavLink, NavMenu } from "./commit-ui/components/Navbar"
+export { Input } from "./commit-ui/components/Input"
+export type { ExtendedInputProps as InputProps } from "./commit-ui/components/Input"
+export { Select } from "./commit-ui/components/Select"
+export type { SelectProps } from "./commit-ui/components/Select"
+export { Modal } from "./commit-ui/components/Modal"
+export type { ModalProps } from "./commit-ui/components/Modal"
+export { Card } from "./commit-ui/components/Card"
+export type { CardProps } from "./commit-ui/components/Card"
+export { Alert } from "./commit-ui/components/Alert"
